7c0aa4444f65b3ca5ffd22df655db248cb2cbcb1
quentincrida <qcrida@gmail.com>
2020-04-21 06:22:18 +0100

    RESTful controller added

---
+++ src/main/java/com/example/techconnect/pirateService/controllers/PirateController.java
@@ -0,0 +1,28 @@
+package com.example.techconnect.pirateService.controllers;
+
+import com.example.techconnect.pirateService.models.Pirate;
+import com.example.techconnect.pirateService.repositories.PirateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PirateController {
+    @Autowired
+    PirateRepository pirateRepository;
+
+    @GetMapping(value = "/pirates")
+    public List<Pirate> getAllPirates() {
+        return pirateRepository.findAll();
+    }
+
+    @GetMapping(value = "/pirates/{id}")
+    public Optional<Pirate> getPirate(@PathVariable Long id) {
+        return pirateRepository.findById(id);
+    }
+
+}
